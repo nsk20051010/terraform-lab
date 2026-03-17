b980171bdb307030f3cb9c4509887c61164f14d5
Nishad Kadam <nishad20051@gmail.com>
2026-03-17 14:44:02 +0000

    Day3: Docker container provisioning using Terraform (nginx on port 8080)

---
+++ day3/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx_container" {
+  name  = "nginx_container"
+  image = docker_image.nginx.name
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
+
+output "service_url" {
+  value = "http://localhost:8080"
+}
